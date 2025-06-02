88a2eb4991abc417e8c124c75559637489700c64
MarioTiara <mario.tiarapratama@gmail.com>
2025-06-02 21:59:36 +0700

    add swagger

--- src/SeatBooking.Web/Program.cs
+++ src/SeatBooking.Web/Program.cs
@@ -1,4 +1,3 @@
-
 using SeatBooking.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,17 +5,31 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Add DbContext
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddScoped<SeatMapService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    // Enable Swagger in development
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SeatBooking API v1");
+        c.RoutePrefix = "swagger"; // Serve Swagger UI at /swagger
+    });
 }
 
 app.UseHttpsRedirection();
